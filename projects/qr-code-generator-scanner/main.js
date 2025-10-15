597ccfdfb3d0fb3404293666f139ebe436366105
Venisha Kalola <venishakalola@gmail.com>
2025-10-15 15:15:56 +0530

    added new projects

---
+++ projects/qr-code-generator-scanner/main.js
@@ -0,0 +1,14 @@
+// TODO: Generate QR code from input text/URL
+// TODO: Display generated QR code
+// TODO: Download QR code image
+// TODO: Scan uploaded QR code image
+// TODO: Display scanned QR code result
+
+function initQRCodeGeneratorScanner() {
+  // TODO: Generate QR code from input
+  // TODO: Scan QR code from image
+  // TODO: Display results
+  // TODO: Download QR code
+}
+
+window.addEventListener('DOMContentLoaded', initQRCodeGeneratorScanner);
